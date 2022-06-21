c5c076217993227e5da301e8a4dc565ffe462af4
thalita <t.vergilio@leedsbeckett.ac.uk>
2022-06-21 13:01:53 +0100

    Added more unit test for Circle. Fixed bugs. Added validation and exception handling.

--- src/main/java/uk/ac/leedsBeckett/ase/model/Circle.java
+++ src/main/java/uk/ac/leedsBeckett/ase/model/Circle.java
@@ -1,8 +1,13 @@
 package uk.ac.leedsBeckett.ase.model;
 
+import javafx.geometry.Point2D;
+
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public class Circle extends javafx.scene.shape.Circle {
+    public static final Point2D DEFAULT_POSITION = new Point2D(250d, 150d);
+    public static final double DEFAULT_RADIUS = 100d;
 
     public Circle(double radius) {
         super(radius);
@@ -20,13 +25,15 @@ public class Circle extends javafx.scene.shape.Circle {
                 return new Circle(parameters.get(0), parameters.get(1), parameters.get(2));
             }
             case 2: {
-
+                return new Circle(parameters.get(0), parameters.get(1), DEFAULT_RADIUS);
             }
             case 1: {
-                return new Circle(parameters.get(0));
+                return new Circle(DEFAULT_POSITION.getX(), DEFAULT_POSITION.getY(), parameters.get(0));
             }
-            default: {
-                return new Circle(250d, 150d, 100d);
+            case 0: {
+                return new Circle(DEFAULT_POSITION.getX(), DEFAULT_POSITION.getY(), DEFAULT_RADIUS);
+            } default: {
+                throw new InvalidParameterException("You must pass a maximum of four parameters.");
             }
 
         }
--- src/test/java/uk/ac/leedsBeckett/ase/model/CircleTest.java
+++ src/test/java/uk/ac/leedsBeckett/ase/model/CircleTest.java
@@ -2,21 +2,53 @@ package uk.ac.leedsBeckett.ase.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
 
-    private final double xPosition = 10.0;
-    private final double yPosition = 20.0;
-    private final double radius = 30.0;
+    private final double testXPosition = 10.0;
+    private final double testYPosition = 20.0;
+    private final double testRadius = 30.0;
 
     @Test
     void createCircle_withThreeParameters_createsCircleCorrectly() {
-        Circle circle = Circle.createCircle(Arrays.asList(xPosition, yPosition, radius));
-        assertEquals(xPosition, circle.getCenterX());
-        assertEquals(yPosition, circle.getCenterY());
-        assertEquals(radius, circle.getRadius());
+        Circle circle = Circle.createCircle(Arrays.asList(testXPosition, testYPosition, testRadius));
+        assertEquals(testXPosition, circle.getCenterX());
+        assertEquals(testYPosition, circle.getCenterY());
+        assertEquals(testRadius, circle.getRadius());
+    }
+
+    @Test
+    void createCircle_withTwoParameters_createsCircleUsingDefaultRadius() {
+        Circle circle = Circle.createCircle(Arrays.asList(testXPosition, testYPosition));
+        assertEquals(testXPosition, circle.getCenterX());
+        assertEquals(testYPosition, circle.getCenterY());
+        assertEquals(Circle.DEFAULT_RADIUS, circle.getRadius());
+    }
+
+    @Test
+    void createCircle_withOneParameter_createsCircleUsingDefaultPosition() {
+        Circle circle = Circle.createCircle(List.of(testRadius));
+        assertEquals(Circle.DEFAULT_POSITION.getX(), circle.getCenterX());
+        assertEquals(Circle.DEFAULT_POSITION.getY(), circle.getCenterY());
+        assertEquals(testRadius, circle.getRadius());
+    }
+
+    @Test
+    void createCircle_withNoParameters_createsCircleUsingDefaultRadiusAndPosition() {
+        Circle circle = Circle.createCircle(List.of());
+        assertEquals(Circle.DEFAULT_POSITION.getX(), circle.getCenterX());
+        assertEquals(Circle.DEFAULT_POSITION.getY(), circle.getCenterY());
+        assertEquals(Circle.DEFAULT_RADIUS, circle.getRadius());
+    }
+
+    @Test
+    void createCircle_withFourParameters_throwsException() {
+        assertThrows(InvalidParameterException.class, () -> Circle.createCircle(Arrays.asList(1.0, 2.0, 3.0, 4.0)),
+                "Exception was not thrown.");
     }
 }
